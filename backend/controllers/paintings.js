66dd9ce623b0e625d77da00cf0cd9d2644389fbf
Berra Karaman <esma.berra.karaman@hotmail.com>
2024-05-17 15:08:17 +0300

    implement the logic for the /last-id endpoint

--- backend/controllers/paintings.js
+++ backend/controllers/paintings.js
@@ -2,7 +2,15 @@ const paintingsRouter = require('express').Router()
 const paintings = require('../data/paintings');
 
 paintingsRouter.get('/last-id', async (request, response) => {
+  const lastActivePainting = paintings
+    .sort((a, b) => b.id - a.id)
+    .find(painting => painting.isActive)
 
+  if (lastActivePainting) {
+    response.status(200).json({ lastId: lastActivePainting.id })
+  } else {
+    response.status(404).json({ error: 'No active painting found' })
+  }
 })
 
 paintingsRouter.get('/:id', async (request, response) => {
