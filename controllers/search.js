2e543fe2fae993f6da0ef528d3e4f7179d9848ab
Berra Karaman <esma.berra.karaman@hotmail.com>
2024-05-30 16:12:39 +0300

    add autocomplete router

--- controllers/search.js
+++ controllers/search.js
@@ -1,38 +1,66 @@
 const searchRouter = require('express').Router();
 const Artwork = require('../models/artwork');
 
-searchRouter.get('/:search', async (request, response) => {
-  const query = request.params.search;
-
-  let searcher_aggregate = {
-    $search: {
-      index: "search_painting",
-      compound: {
-        must: [
-          {
-            text: {
-              query: query,
-              path: "title",
-              fuzzy: {},
+searchRouter.get("/:search", async (request, response) => {
+  const results = await Artwork.aggregate([
+    {
+      $search: {
+        index: "search_painting",
+        compound: {
+          must: [
+            {
+              text: {
+                query: request.params.search,
+                path: "title",
+                fuzzy: {},
+              },
             },
-          },
-        ],
+          ],
+        },
       },
     },
-  };
-
-  let projection = {
-    $project: {
-      _id: 0,
-      title: 1,
+    {
+      $limit: 20,
     },
-  };
+    {
+      $project: {
+        _id: 0,
+        title: 1,
+      },
+    },
+  ]);
 
-  let results = await Artwork.aggregate([searcher_aggregate, projection]).limit(
-    20
-  );
   response.status(200).send(results);
-})
+});
 
+searchRouter.get("/autocomplete/:search", async (request, response) => {
+  const results = await Artwork.aggregate([
+    {
+      $search: {
+        index: "autocomplete_painting",
+        autocomplete: {
+          query: request.params.search,
+          path: "title",
+        },
+        highlight: {
+          path: ["title"],
+        },
+      },
+    },
+    {
+      $limit: 20,
+    },
+    {
+      $project: {
+        title: 1,
+        highlights: {
+          $meta: "searchHighlights",
+        },
+      },
+    },
+  ]);
+
+  response.status(200).send(results);
+});
 
 module.exports = searchRouter
